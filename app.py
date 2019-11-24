32d51d178730716f64f5aa1c72ed495da8197d16
dr31 <andre.daher@blockforce.in>
2019-11-24 14:17:59 -0300

    Implementação das páginas 'empresas' e 'eventos'

--- app.py
+++ app.py
@@ -34,17 +34,76 @@ def eventos():
         conn = psycopg2.connect(DSN)
         cur = conn.cursor()
         try:
-            cur.execute("INSERT INTO EVENTO (ano, titulo) VALUES (%s, %s)", (ano, titulo))
-            conn.commit()
+            # Verificar se o ano já está cadastrado
+            cur.execute("SELECT ano FROM EVENTO WHERE ano={}".format(ano))
+            data = cur.fetchone()
+            if data is None:
+                # Ano ainda não estava cadastrado
+                cur.execute("INSERT INTO EVENTO (ano, titulo) VALUES (%s, %s)", (ano, titulo))
+                conn.commit()
+                response = jsonify({
+                    'ano': ano,
+                    'titulo': titulo
+                })
+                code = 200
+            else:
+                response = jsonify({
+                    'error': 'Ano já cadastrado'
+                })
+                code = 422
         except psycopg2.Error as e:
-            print(e)
-            
+            response = jsonify({
+                'error': 'Ocorreu um erro inesperado'
+            })
+            code = 500
+        
         conn.close()
+
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response, code
+
+
+@app.route('/api/lista_eventos', methods=['GET'])
+def lista_eventos():
+    conn = psycopg2.connect(DSN)
+    cur = conn.cursor()
+    try:
+        # Verificar se o ano já está cadastrado
+        cur.execute("SELECT * FROM EVENTO")
+        data = cur.fetchall()
         response = jsonify({
-            'ano': ano,
-            'titulo': titulo
+            'data': data
         })
         code = 200
+    except:
+        response = jsonify({
+            'error': 'Ocorreu um erro inesperado'
+        })
+        code = 500
+    conn.close()
+
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response, code
+
+@app.route('/api/empresas', methods=['GET'])
+def empresas():
+    ano = request.args.get("ano")
+    conn = psycopg2.connect(DSN)
+    cur = conn.cursor()
+    try:
+        # Verificar se o ano já está cadastrado
+        cur.execute("SELECT empresa.CNPJ, empresa.NOME, empresa.SETOR, participa.TOTAL_PAGO, participa.NOTA_FEEDBACK FROM PARTICIPA JOIN EMPRESA on (participa.empresa = empresa.cnpj) WHERE participa.evento={}".format(ano))
+        data = cur.fetchall()
+        response = jsonify({
+            'data': data
+        })
+        code = 200
+    except:
+        response = jsonify({
+            'error': 'Ocorreu um erro inesperado'
+        })
+        code = 500
+    conn.close()
 
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response, code
